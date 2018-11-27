8720d42066f30393136f733bc187fc348d2caf37
Michał Siejak <michal@siejak.pl>
2018-11-27 22:02:53 +0100

    Add master include file

---
+++ src/rsm/rsm.hpp
@@ -0,0 +1,19 @@
+/*
+ * rsm :: Random Sampling Mathematics
+ * Copyright (c) 2018 Michał Siejak
+ * Released under the MIT license; see LICENSE file for details.
+ */
+
+#pragma once
+
+#include "init.hpp"
+#include "lds.hpp"
+#include "next.hpp"
+#include "options.hpp"
+#include "range.hpp"
+#include "utils.hpp"
+
+#include "generators/pcg32.hpp"
+
+#include "samplers/halton.hpp"
+#include "samplers/stratified.hpp"
